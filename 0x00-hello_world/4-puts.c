772879b2139fbe19e166159cb594664df6976c88
Tarie123 <tarymhlang@gmail.com>
2022-12-08 15:06:56 -0800

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /*
- * main-mainblock
+ * main - mainblock
  * Return=0
  *
  */
 int main (void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
