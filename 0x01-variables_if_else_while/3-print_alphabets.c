a17adcee0b5aca316898af4c453272f678f0a31b
Tarie123 <tarymhlang@gmail.com>
2022-12-12 11:57:58 -0800

    Alphabet2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,4 @@
-#include <stdio.h>                                                                                                              
-
-                                                                                                                                
-
+#include <stdio.h>
  /**                                                                                                                            
   * main - main block                                                                                                        
   *
