47afc354e90aaac7f2eb62d5399d1d37ef3865a8
Tarie123 <tarymhlang@gmail.com>
2022-12-12 13:16:19 -0800

    Alphabet2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,29 +8,23 @@
   */
 int main(void)
 {
-
- char c = 'a';
+char c = 'a';
 while (c <= 'z')
-
- {
-
-	putchar(c);
-        c++;
-
+{
+putchar(c);
+c++;
 	}
 
      c = 'A';
-
 while (c <= 'Z')
   {
-	  putchar(c);
-        c++;
+putchar(c);
+c++;
 
 }
+putchar('\n');
 
-  putchar('\n');
-
-  return (0);
+return (0);
 
 }
 
