8ac1c2ece213e15454c0d5b2cfdb13ddf6f4098c
Tarie123 <tarymhlang@gmail.com>
2022-12-12 00:39:24 -0800

    Size

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  * Return: 1
  */
 int main(void)
-{   int s = strlen(
-		"and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+{   int s = strlen(" ");
 			write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 					, s);
 
