5329e08f6c8371ae5bacf35fffdf1141af6a52be
Tarie123 <tarymhlang@gmail.com>
2022-12-13 14:54:10 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - printing the aplhabert
+ * Return: 0(success)
+ */
+void print_alphabet(void)
+char A = 'a';
+{
+while (A <= 'z')
+{
+_putchar(A);
+A++;
+}
+}
+_putchar('\n');
+return 0;
+}
