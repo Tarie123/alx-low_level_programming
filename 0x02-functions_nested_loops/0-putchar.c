1e544cf0aa9f7dff2cb9a6fafa27c57bd1f71197
Tarie123 <tarymhlang@gmail.com>
2022-12-13 13:43:39 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - Print "Holberton" followed by a new line.
+ * Return: 0(success)
+ */
+int main(void)
+{
+char holberton[] = "Holberton";
+int i = 0;
+while (holberton[i] != '\0')
+{
+_putchar(holberton[i]);
+i++;
+}
+_putchar('\n');
+{return (0);
+}
