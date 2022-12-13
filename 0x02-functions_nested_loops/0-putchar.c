c758fb7467d511437aa0aa30bfbd54f77a7d89b4
Tarie123 <tarymhlang@gmail.com>
2022-12-13 14:45:00 -0800

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-char _putchar[] = "_putchar";
+char main[] = "_putchar";
 int i = 0;
-while (_putchar[i] != '\0')
+while (main[i] != '\0')
 {
-_putchar (_putchar[i]);
+_putchar (main[i]);
 i++;
 }
 _putchar('\n');
