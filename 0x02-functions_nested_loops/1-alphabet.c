48286943c146838dc6ff1d2757152c9e9b3df882
Tarie123 <tarymhlang@gmail.com>
2022-12-19 11:38:24 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,23 @@
  * Return: 0(success)
  */
 void print_alphabet(void)
- char c = 'a';
+
 {
-while (c <= 'z')
-{
-_putchar(c);
-c++;
-}
-}
-_putchar('\n');
-return 0;
+
+		char c = 'a';
+
+
+
+			while (c <= 'z')
+
+					{
+
+								_putchar(c);
+
+										c++;
+
+											}
+
+				_putchar('\n');
+
 }
