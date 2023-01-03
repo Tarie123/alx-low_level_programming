98203bfe42ff012fc242d54370c829dad1629425
Tarie123 <tarymhlang@gmail.com>
2023-01-03 10:35:23 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @n: number of bytes to be changed
  * Return: a pointer to memory area s
  */
-char *_memset(char *s, char b, unsigned int n)
+char*_memset(char*s, char b, unsigned int n)
 {
 int i = 0;
 for (; n > 0 ; i++)
