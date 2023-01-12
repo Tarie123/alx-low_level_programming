c7e312aae9978aca2b564b1345626f2522cc529a
Tarie123 <tarymhlang@gmail.com>
2023-01-12 07:14:50 -0800

    101-strtow.c

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * get_words_count - Computes the number of words in a string
+ * @str: The source string
+ * @count: The number of words
+ */
+void get_words_count(char *str, int *count)
+{
+int i, j;
+char prev_char = ' ';
+for (i = 0; str != NULL && *(str + i) != '\0'; i++)
+{
+if (*(str + i) != ' ' && prev_char == ' ')
+{
+*count += 1;
+j = 0;
+}
+if (*(str + i) != ' ')
+j++;
+prev_char = *(str + i);
+}
+}
+void get_word_length(char *str, int *length)
+{
+*length = 0;
+if (str == NULL || (str != NULL && (*str == '\0' || *str == ' ')))
+return;
+while (*(str + *length) != '\0' && *(str + *length) != ' ')
+*length += 1;
+}
+char **strtow(char *str)
+{
+char **words;
+int i, j, k, len, words_count;
+char prev_char = ' ';
+get_words_count(str, &words_count);
+if (str == NULL || *str == '\0' || words_count == 0)
+return (NULL);
+words = malloc((sizeof(char *) * (words_count + 1)));
+if (words)
+{
+j = -1;
+k = 0;
+prev_char = ' ';
+for (i = 0; *(str + i) != '\0'; i++)
+{
+if (*(str + i) != ' ')
+{
+if (prev_char == ' ')
+{
+j++;
+get_word_length(str + i, &len);
+*(words + j) = malloc((sizeof(char) * (len + 1)));
+if (*(words + j) == NULL)
+return (NULL);
+k = 0;
+}
+*(*(words + j) + k) = *(str + i);
+k++;
+if (*(str + i + 1) == '\0' || *(str + i + 1) == ' ')
+*(*(words + j) + k) = '\0';
+}
+prev_char = *(str + i);
+}
+*(words + j + 1) = NULL;
+return (words);
+}
+return (NULL);
+}
